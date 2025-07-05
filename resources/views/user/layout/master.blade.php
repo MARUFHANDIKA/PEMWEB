0352ff998b6839fde3b7e3b074889c4a4c59134b
MARUFHANDIKA <marufhandika9@gmail.com>
2025-07-05 09:39:30 +0700

    header footer user

--- resources/views/user/layout/master.blade.php
+++ resources/views/user/layout/master.blade.php
@@ -32,53 +32,65 @@
 
 <body>
     <!-- Navbar Start -->
-    <div class="container-fluid bg-dark mb-3">
-        <div class="container px-0">
-            <nav class="navbar navbar-expand-lg navbar-dark py-3">
-                <a class="navbar-brand fw-bold" href="{{ route('user#home') }}">
-                    <i class="mie-ayam"></i> mie ayam bakar pakde joyo
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link {{ Request::routeIs('user#home') ? 'active' : '' }}"
-                                href="{{ route('user#home') }}">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link {{ Request::routeIs('user#carts') ? 'active' : '' }}"
-                                href="{{ route('user#carts') }}">Cart</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link {{ Request::routeIs('user#history') ? 'active' : '' }}"
-                                href="{{ route('user#history') }}">History</a>
-                        </li>
-                    </ul>
-                    <div class="dropdown">
-                        <button class="btn btn-outline-light dropdown-toggle" type="button" data-bs-toggle="dropdown">
+ <header class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+    <div class="container">
+        <a class="navbar-brand d-flex align-items-center" href="{{ route('user#home') }}">
+            <img src="{{ asset('images/Logo.png') }}" alt="Logo Pakde Joyo" height="50" class="me-2">
+            <span class="fw-bold">Mie Ayam Bakar Pakde Joyo</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+
+                @auth
+                    <li class="nav-item">
+                        <a class="nav-link {{ Request::routeIs('user#home') ? 'active fw-bold text-primary' : '' }}"
+                           href="{{ route('user#home') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ Request::routeIs('user#carts') ? 'active fw-bold text-primary' : '' }}"
+                           href="{{ route('user#carts') }}">Cart</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ Request::routeIs('user#history') ? 'active fw-bold text-primary' : '' }}"
+                           href="{{ route('user#history') }}">History</a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle fw-bold" href="#" role="button" data-bs-toggle="dropdown">
                             <i class="fa-solid fa-user me-1"></i> {{ Auth::user()->name }}
-                        </button>
+                        </a>
                         <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="{{ route('user#view') }}"><i
-                                        class="fa-solid fa-user"></i> Account</a></li>
-                            <li><a class="dropdown-item" href="{{ route('user#changePassword') }}"><i
-                                        class="fa-solid fa-lock"></i> Change Password</a></li>
+                            <li><a class="dropdown-item" href="{{ route('user#view') }}">
+                                <i class="fa-solid fa-user"></i> Akun</a></li>
+                            <li><a class="dropdown-item" href="{{ route('user#changePassword') }}">
+                                <i class="fa-solid fa-lock"></i> Ubah Password</a></li>
                             <li>
-                                <form action="{{ route('logout') }}" method="post" class="px-3">
+                                <form action="{{ route('logout') }}" method="POST" class="px-3">
                                     @csrf
-                                    <button class="btn btn-sm btn-danger w-100 mt-2" type="submit">
+                                    <button type="submit" class="btn btn-sm btn-danger w-100 mt-2">
                                         <i class="fa-solid fa-right-from-bracket"></i> Logout
                                     </button>
                                 </form>
                             </li>
                         </ul>
-                    </div>
-                </div>
-            </nav>
+                    </li>
+                @else
+                    <li class="nav-item"><a class="nav-link" href="#menu">Menu</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#about">Tentang Kami</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contact">Kontak</a></li>
+                    <li class="nav-item">
+                        <a class="btn btn-danger ms-3" href="{{ route('auth#loginPage') }}">Login</a>
+                    </li>
+                @endauth
+
+            </ul>
         </div>
     </div>
+</header>
+
     <!-- Navbar End -->
 
     <!-- Page Content -->
@@ -86,13 +98,35 @@
         @yield('content')
     </main>
 
-    <!-- Footer (Optional - Uncomment to use) -->
-    {{--
-    <footer class="bg-dark text-white text-center py-3 mt-5">
-        <small>&copy; {{ date('Y') }} Store. All rights reserved.</small>
-    </footer>
-    --}}
-
+ <!-- resources/views/partials/footer.blade.php -->
+<footer class="bg-dark text-white py-4 mt-5">
+    <div class="container row mx-auto">
+        <div class="col-md-3">
+            <h5>Mie Ayam Bakar Pakde Joyo</h5>
+            <p>Melayani dengan cita rasa autentik sejak 2010</p>
+        </div>
+        <div class="col-md-3">
+            <h5>Menu</h5>
+            <a href="{{ route('user#home') }}" class="d-block text-white">Home</a>
+            <a href="{{ route('user#home') }}#menu" class="d-block text-white">Menu</a>
+            <a href="{{ route('user#home') }}#about" class="d-block text-white">Tentang Kami</a>
+            <a href="{{ route('user#home') }}#contact" class="d-block text-white">Kontak</a>
+        </div>
+        <div class="col-md-3">
+            <h5>Kontak</h5>
+            <p><i class="fa fa-phone me-1"></i> +62 882-0089-01178</p>
+            <p><i class="fa fa-envelope me-1"></i> info@mieayampakdejoyo.com</p>
+            <p><i class="fa fa-map-marker-alt me-1"></i> Jl. Raya No. 123, Kota</p>
+        </div>
+        <div class="col-md-3">
+            <h5>Ikuti Kami</h5>
+            <a href="https://www.instagram.com/mie_ayam_pakde_joyo?igsh=eTUwMm54eG4wOXMz " target="_blank" class="d-block text-white">Instagram</a>
+            <a href="https://maps.app.goo.gl/PnJzTMjiqgRjfLR9A?g_st=aw" target="_blank" class="d-block text-white">Gmaps</a>
+        </div>
+    </div>
+    <div class="text-center mt-4">&copy; 2025 Mie Ayam Bakar Pakde Joyo. All rights reserved.</div>
+</footer>
+    <!-- Footer End -->
     <!-- Back to Top Button -->
     <a href="#" class="btn btn-primary position-fixed bottom-0 end-0 m-4"><i class="fa fa-angle-double-up"></i></a>
 
